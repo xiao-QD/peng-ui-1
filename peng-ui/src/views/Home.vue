01a1c4be94feaed0f0c3773b5d06f75431f7b805
peng <1422581843@qq.com>
2022-10-06 22:50:21 +0800

    搭建了html结构

---
+++ peng-ui/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>PengUI</h1>
+      <h2>一个厉害的UI框架</h2>
+      <p class="actions">
+        <a href="">github</a>
+        <a href="">开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.topnar
+</style>
