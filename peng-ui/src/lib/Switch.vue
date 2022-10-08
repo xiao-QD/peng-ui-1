bfb1ee5da3b085c9b9fd1471fca73481cf0c6552
peng <1422581843@qq.com>
2022-10-08 11:12:55 +0800

    按钮切换实现动画效果

--- peng-ui/src/lib/Switch.vue
+++ peng-ui/src/lib/Switch.vue
@@ -32,6 +32,7 @@ span {
   width: $h2;
   background: white;
   border-radius: $h2 / 2;
+  transition: left 250ms;
 }
 button.checked {
   background: blue;
@@ -39,5 +40,8 @@ button.checked {
 
 button.checked > span {
   left: calc(100% - #{$h2} - 2px);
+}
+button:focus {
+  outline: none;
 }
 </style>
