d2aa50e10bb588d1b6ff9f3edf94ad44ef910bbf
peng <1422581843@qq.com>
2022-10-09 00:06:33 +0800

    为按钮添加theme属性

--- peng-ui/src/lib/Button.vue
+++ peng-ui/src/lib/Button.vue
@@ -1,9 +1,16 @@
 <template>
-  <button>
+  <button class="peng-button" :class="`theme-${theme}`">
     <slot></slot>
   </button>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+defineProps({
+  theme: {
+    type: String,
+    default: "button",
+  },
+});
+</script>
 
 <style scoped></style>
