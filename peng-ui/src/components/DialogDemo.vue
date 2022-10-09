10066cc9e387a0d7c87465fc86ac6db90af82d11
peng <1422581843@qq.com>
2022-10-09 22:12:18 +0800

    实现点击按钮弹出dialog

--- peng-ui/src/components/DialogDemo.vue
+++ peng-ui/src/components/DialogDemo.vue
@@ -1,10 +1,15 @@
 <template>
   <h1>示例1</h1>
-  <Dialog></Dialog>
+  <button @click="x = !x">弹窗</button>
+  <Dialog :visible="x"></Dialog>
 </template>
 
 <script setup lang="ts">
 import Dialog from "../lib/Dialog.vue";
+import Button from "../lib/Button.vue";
+import { ref } from "vue";
+
+const x = ref(false);
 </script>
 
 <style scoped></style>
