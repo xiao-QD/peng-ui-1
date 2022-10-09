24e2fc88aaa99ea2689e48812f7305f717cfe363
peng <1422581843@qq.com>
2022-10-09 21:27:56 +0800

    初步创建dialog组件

--- peng-ui/src/components/DialogDemo.vue
+++ peng-ui/src/components/DialogDemo.vue
@@ -1,7 +1,10 @@
 <template>
-  <div>dialog</div>
+  <h1>示例1</h1>
+  <Dialog></Dialog>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Dialog from "../lib/Dialog.vue";
+</script>
 
 <style scoped></style>
---
+++ peng-ui/src/lib/Dialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="dialog-overlay"></div>
+  <div class="dialog-wrapper">
+    <header></header>
+    <main>
+      <p>1</p>
+      <p>2</p>
+    </main>
+    <footer>
+      <Button level="main">确定</Button>
+      <Button level="main">取消</Button>
+    </footer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Button from "./Button.vue";
+</script>
+
+<style scoped></style>
