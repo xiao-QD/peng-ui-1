e90f7965b55c31178f9482cb75998155f9a9a187
xiao-QD <1422581843@qq.com>
2022-10-12 22:15:28 +0800

    1

--- peng-ui/src/lib/Tabs.vue
+++ peng-ui/src/lib/Tabs.vue
@@ -1,7 +1,21 @@
 <template>
-  <div>tabs</div>
+  <!-- <div>tabs</div>
+  <button @click="show">打印</button> -->
+  <slot></slot>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { useSlots, getCurrentInstance, onMounted } from "vue";
+const slotaa = useSlots();
+const Curr = getCurrentInstance();
+// const show = () => {
+//   console.log(slotaa);
+//   //   console.log(Curr);
+// };
+
+onMounted(() => {
+  console.log(slotaa);
+});
+</script>
 
 <style scoped></style>
