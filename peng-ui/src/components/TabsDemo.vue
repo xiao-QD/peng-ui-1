be7d866f90f23556d11d91d7b76cb50ff65b0030
peng <1422581843@qq.com>
2022-10-12 14:52:34 +0800

    创建了Tabs组件和Tab组件

--- peng-ui/src/components/TabsDemo.vue
+++ peng-ui/src/components/TabsDemo.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>tabstabstabstabstabstabstabstabstabstabstabstabstabstabs</div>
+  <div>Tabs示例</div>
+  <h1>示例1</h1>
+  <Tabs>
+    <Tab title="导航1"></Tab>
+    <Tab title="导航2"></Tab>
+  </Tabs>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Tab from "../lib/Tab.vue";
+import Tabs from "../lib/Tabs.vue";
+</script>
 
 <style lang="scss" scoped></style>
---
+++ peng-ui/src/lib/Tab.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>我是tab</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
